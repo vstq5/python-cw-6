57a3931bcc9b5c9f2ebb41882e9842e3c1edda90
vstq5 <ghanmly036@gmail.com>
2023-09-06 19:25:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # write your code here
+person = {
+'name': 'ali',
+'age': '18',
+'hobbies': ['video games' , 'football']
+}
+person["age"] = 20
+person["country"] = "kuwait"
+person["hobbies"] = 'video games' , 'football'
+
+def check_hobbies(person):
+    if len(person['hobbies']) >= 3:
+        print(f"wow you are amazing")
+
+check_hobbies(person)
